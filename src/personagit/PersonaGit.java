1ff3c15d25311c4b12c7a758dabe1c9f9aa2c75e
alfonso lara <orozcoenrique159@gmail.com>
2024-08-19 19:17:03 -0500

    primer commit en clase persona

---
+++ src/personagit/PersonaGit.java
@@ -0,0 +1,11 @@
+package personagit;
+
+public class PersonaGit {
+
+    public static void main(String[] args) {
+        
+        Persona persona1 = new Persona("Alexa", 55);
+        persona1.Saludar();
+    }
+    
+}
